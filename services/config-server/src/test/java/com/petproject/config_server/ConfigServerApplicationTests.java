5856f18de0c43e558cba1ed56eb78ac23b64f67f
Andrii <stasaaass@gmail.com>
2026-02-16 01:03:31 +0100

    Add CongigServerApp, also fixed docker-compose properties

---
+++ services/config-server/src/test/java/com/petproject/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
