f34ddddc6729c1ac8233881b3a78bcaba686f788
Andrii <stasaaass@gmail.com>
2026-02-17 16:26:15 +0100

    Add logic to curtomer-service

---
+++ services/customer/src/main/java/com/petproject/ecommerce/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.petproject.ecommerce.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+}
