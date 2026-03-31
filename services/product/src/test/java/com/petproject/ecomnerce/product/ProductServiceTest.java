e01816a18e7cd78d05b29529968be4888e2bc726
Andrii-Hamza <168895938+Andrii-Hamza@users.noreply.github.com>
2026-03-31 21:16:52 +0300

    Add Unit5 for createProduct method in ProductService class

--- services/product/src/test/java/com/petproject/ecomnerce/product/ProductServiceTest.java
+++ services/product/src/test/java/com/petproject/ecomnerce/product/ProductServiceTest.java
@@ -10,16 +10,22 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+//import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("ProductService Unit Tests")
 class ProductServiceTest {
 
     @Mock // add mock, because i dont want interact with db
-    private ProductRepository repository;
+    private ProductRepository productRepository;
     @Mock
-    private ProductMapper mapper;
+    private ProductMapper productMapper;
 
     @InjectMocks
     private ProductService productService; // testing class(object)
@@ -30,32 +36,54 @@ class ProductServiceTest {
 
     private Category productCategory;
     private Product testProduct;
-    private ProductRequest productRequest;
+    private ProductRequest testProductRequest;
 
     @BeforeEach
     void setUp () {
 
-        //todo
-//        this.productCategory = Category.builder()
-//                .id(123)
-//                .name("Work")
-//                .description("Work related ")
-//                .build()
+        this.productCategory = Category.builder()
+                .id(123)
+                .name("Work")
+                .description("Work related Products")
+                .products(null)
+                .build();
 
-//        this.testProduct = Product.builder()
-//                .
-//                .build();
+        this.testProduct = Product.builder()
+                .id(321)
+                .name("Test product")
+                .description("Test description")
+                .availableQuantity(2)
+                .price(new BigDecimal("19.99"))
+                .category(this.productCategory)
+                .build();
+
+        this.testProductRequest = new ProductRequest(
+                321,
+                "Test product",
+                "Test description",
+                5,
+                new BigDecimal("59.99"),
+                123
+        );
 
     }
 
         @Test
         @DisplayName("Should create Product successfully when valid request exists")
         void shouldCreateProductSuccessfully() {
+
             // Given
+            when(productMapper.toProduct(testProductRequest)).thenReturn(testProduct);
+            when(productRepository.save(any(Product.class))).thenReturn(testProduct);
 
             // When
+            final Integer result  = ProductServiceTest.this.productService.createProduct(testProductRequest);
 
             // Then
+            assertNotNull(result);
+            assertEquals(321, result);
+            verify(productMapper, times(1)).toProduct(testProductRequest);
+            verify(productRepository, times(1)).save(testProduct);
         }
     }
 }
